8503ebebf141087282418fda8ff1ba1ac7ac9a53
dasakash26 <akashd2664@gmail.com>
2025-08-29 20:28:20 +0530

    implement PropertyNFT contract

--- contracts/src/Contract.sol
+++ contracts/src/Contract.sol
@@ -1,4 +1,28 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-contract Contract { }
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PropertyNFT is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+
+    constructor() ERC721("Willow", "WLW") Ownable(msg.sender) {
+        nextTokenId = 0;
+    }
+
+    function mintProperty(
+        address to,
+        string memory tokenURI
+    ) public onlyOwner returns (uint256) {
+        uint256 newId = nextTokenId;
+        _safeMint(to, newId);
+        _setTokenURI(newId, tokenURI);
+        nextTokenId++;
+        return newId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return nextTokenId;
+    }
+}
